25f95456ce3020eae4abe1b1b5f5ed447438e5c5
Nkemjika Obi <nkemjikaobi@gmail.com>
2024-01-29 23:14:20 -0500

    Add username, password and email fields alongside save button

--- app/src/main/java/com/example/mapd721_a1_nkemjikaobi/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a1_nkemjikaobi/MainActivity.kt
@@ -3,14 +3,35 @@ package com.example.mapd721_a1_nkemjikaobi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mapd721_a1_nkemjikaobi.ui.theme.MAPD721A1NkemjikaObiTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +43,93 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+
+    var username by remember { mutableStateOf( "") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        //Username Label
+        Text(
+            modifier = Modifier
+                .padding(16.dp, top = 30.dp),
+            text = "Username",
+            color = Color.Gray,
+            fontSize = 12.sp
+        )
+
+        //Username input field
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            value = username,
+            onValueChange = {  },
+        )
+
+        //Email Label
+        Text(
+            modifier = Modifier
+                .padding(16.dp, top = 30.dp),
+            text = "Email",
+            color = Color.Gray,
+            fontSize = 12.sp
+        )
+
+        //Email input field
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            value = email,
+            onValueChange = {  },
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            value = password,
+            onValueChange = {  },
+            label = { Text(text = "Password", color = Color.Gray, fontSize = 12.sp) },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        // save button
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(start = 16.dp, end = 16.dp),
+            onClick = {
+
+            },
+        )
+        {
+            // button text
+            Text(
+                text = "Save",
+                color = Color.White,
+                fontSize = 18.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
     }
 }
 
@@ -41,6 +145,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MAPD721A1NkemjikaObiTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
